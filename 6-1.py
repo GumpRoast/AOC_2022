8f1054d38d66952fef7acd1a88212a1ecadacf1b
GumpRoast <33532206+GumpRoast@users.noreply.github.com>
2024-09-12 00:58:26 +0000

    modified:   6-1.py modified:   6_ex.txt modified:   6_in.txt

--- 6-1.py
+++ 6-1.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 path = str(Path(__file__).parent)
-file_path = path + '/6_ex.txt'
+file_path = path + '/6_in.txt'
 
 with open(file_path, 'r') as f:
     inf = [line.strip() for line in f]
@@ -12,5 +12,19 @@ for item in inf:
     for s in item:
         file.append(s)
 
+# look at each chunk of 4
+
+count = 4
+flag = False
+
 for i in range(len(file) - 3):
-    
+    chunk = file[i:i+4]
+    myset = set(chunk)
+    if len(myset) != len(chunk):
+        count += 1
+        
+    else:
+        break
+
+print(count)
+
